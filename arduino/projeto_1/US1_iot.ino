878dc043fc2451bdb21255ca857a684040c87a9d
Tulio Escarião <tulio.nobrega@ccc.ufcg.edu.br>
2016-07-27 10:25:01 -0300

    Enviando versão correta do secundário

--- arduino/projeto_1/US1_iot.ino
+++ arduino/projeto_1/US1_iot.ino
@@ -1,32 +1,42 @@
 String leitura;
 int temperature;
-
+ 
 #include <Thermistor.h>
 #include <SoftwareSerial.h>
-
+ 
 #define ledRed 2
-#define ledYellow 3 
+#define ledYellow 3
 #define ledGreen 4
-
+ 
 Thermistor temp(A0);
 SoftwareSerial mySerial(10, 11); // RX, TX
-
+ 
 void setup() {
+  Serial.begin(9600);
   mySerial.begin(4800);
-
+ 
   pinMode(ledRed, OUTPUT);
   pinMode(ledGreen, OUTPUT);
   pinMode(ledYellow, OUTPUT);
 }
-
+ 
 void loop() {
-  Serial.println("to aqui oh");
   if (mySerial.available()) {
     leitura = mySerial.readString();
-    Serial.println("JA RECEBI");
+    delay(1);
+    Serial.print("Recebido: ");
+    Serial.println(leitura);
+    
     if (leitura == "TMP") {
+      Serial.println("entendi, enviando temperatura...");
       mySerial.println(temp.getTemp());
-    }/*
+    } else {
+      Serial.print("nao entendi: ");
+      Serial.println(leitura);
+    }
+    delay(1);
+    /*
+    }
     else if (leitura == 'GREEN') {
       digitalWrite(ledGreen, HIGH);
       mySerial.write('OK');  
@@ -37,10 +47,7 @@ void loop() {
     } else {
       digitalWrite(ledRed, HIGH);  
       mySerial.write('OK');
-    }   
+    }  
 */
   }
 }
-
-
-
